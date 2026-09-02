fd101038e70cd1610db2f3938f51a612b3b2fb57
Daniel Gonzalez <danielgonzalezsan4567@gmail.com>
2026-09-02 16:10:39 -0500

    first commit

---
+++ src/main/java/com/carvajal/favorites/service/FavoriteService.java
@@ -0,0 +1,9 @@
+package com.carvajal.favorites.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FavoriteService {
+}
